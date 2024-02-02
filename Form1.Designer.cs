2bcd6d7f97c3d584a4c78de65d4f3a0850c1e68d
Nikiw <Nikiw@NIKIWE_MBHIZA>
2024-02-02 11:59:45 +0200

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,201 @@
+﻿namespace SECTION_B_PRACTICAL_SOLUTION_SUMMATIVE_ASSESSEMENT
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtNewWord = new System.Windows.Forms.TextBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.lblConcatenate = new System.Windows.Forms.Label();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnRemoveItem = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(262, 55);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(108, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Enter a new word";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(277, 213);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(27, 16);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "OR";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(403, 213);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(125, 16);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Select another word";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(18, 213);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(91, 16);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Select a word:";
+            // 
+            // txtNewWord
+            // 
+            this.txtNewWord.Location = new System.Drawing.Point(154, 89);
+            this.txtNewWord.Name = "txtNewWord";
+            this.txtNewWord.Size = new System.Drawing.Size(372, 22);
+            this.txtNewWord.TabIndex = 4;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(21, 241);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(225, 24);
+            this.comboBox1.TabIndex = 5;
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Location = new System.Drawing.Point(341, 241);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(248, 24);
+            this.comboBox2.TabIndex = 6;
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(21, 287);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(80, 20);
+            this.radioButton1.TabIndex = 7;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "Remove";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            this.radioButton1.CheckedChanged += new System.EventHandler(this.radioButton1_CheckedChanged);
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(341, 287);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(80, 20);
+            this.radioButton2.TabIndex = 9;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "Remove";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            this.radioButton2.CheckedChanged += new System.EventHandler(this.radioButton2_CheckedChanged);
+            // 
+            // lblConcatenate
+            // 
+            this.lblConcatenate.AutoSize = true;
+            this.lblConcatenate.BackColor = System.Drawing.Color.White;
+            this.lblConcatenate.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblConcatenate.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.lblConcatenate.Location = new System.Drawing.Point(243, 398);
+            this.lblConcatenate.Name = "lblConcatenate";
+            this.lblConcatenate.Size = new System.Drawing.Size(0, 32);
+            this.lblConcatenate.TabIndex = 10;
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(239, 140);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(148, 23);
+            this.btnAdd.TabIndex = 11;
+            this.btnAdd.Text = "AddNew";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // btnRemoveItem
+            // 
+            this.btnRemoveItem.Location = new System.Drawing.Point(212, 340);
+            this.btnRemoveItem.Name = "btnRemoveItem";
+            this.btnRemoveItem.Size = new System.Drawing.Size(148, 23);
+            this.btnRemoveItem.TabIndex = 12;
+            this.btnRemoveItem.Text = "Remove Item";
+            this.btnRemoveItem.UseVisualStyleBackColor = true;
+            this.btnRemoveItem.Click += new System.EventHandler(this.btnRemoveItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.LightSeaGreen;
+            this.ClientSize = new System.Drawing.Size(705, 450);
+            this.Controls.Add(this.btnRemoveItem);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.lblConcatenate);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.radioButton1);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.txtNewWord);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Words";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtNewWord;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.Label lblConcatenate;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnRemoveItem;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Collections;
+
+namespace SECTION_B_PRACTICAL_SOLUTION_SUMMATIVE_ASSESSEMENT
+{
+    public partial class Form1 : Form
+
+    {
+        private ArrayList addedWords = new ArrayList();
+        private ArrayList concatenatedWords = new ArrayList();
+
+        private HashSet<string> words = new HashSet<string>();
+        public Form1()
+        {
+            InitializeComponent();
+            lblConcatenate.BackColor = System.Drawing.Color.White;
+            lblConcatenate.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            lblConcatenate.Dock = DockStyle.Bottom;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            string newWord = txtNewWord.Text.Trim();
+
+            if (string.IsNullOrWhiteSpace(newWord))
+            {
+                MessageBox.Show("Please enter a word.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            if (words.Contains(newWord))
+            {
+                MessageBox.Show("Word already exists.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            words.Add(newWord);
+            AddWordToComboBoxes(newWord);
+            MessageBox.Show($"Word '{newWord}' has been added.", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            txtNewWord.Clear();
+        }
+        private void AddWordToComboBoxes(string word)
+        {
+            comboBox1.Items.Add(word);
+            comboBox2.Items.Add(word);
+        }
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //string selectedWord1 = comboBox1.SelectedItem as string;
+            //string selectedWord2 = comboBox2.SelectedItem as string;
+
+            //if (string.IsNullOrWhiteSpace(selectedWord1) || string.IsNullOrWhiteSpace(selectedWord2))
+            //{
+            //    MessageBox.Show("Please select two different words.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            //    return;
+            //}
+
+            //string concatenatedWord = selectedWord1 + selectedWord2;
+            //lblConcatenate.Text = $"Concatenated Word: {concatenatedWord}";
+        }
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+            btnRemoveItem.Text = "Remove Item";
+        }
+
+        private void btnRemoveItem_Click(object sender, EventArgs e)
+        {
+            if (radioButton1.Checked)
+            {
+                RemoveSelectedWord();
+            }
+            else
+            {
+                ConcatenateSelectedWords();
+            }
+        }
+
+        private void RemoveSelectedWord()
+        {
+            string selectedWord;
+
+            if (comboBox1.SelectedItem != null)
+            {
+                selectedWord = comboBox1.SelectedItem.ToString();
+                comboBox1.Items.Remove(selectedWord);
+                comboBox2.Items.Remove(selectedWord);
+
+                MessageBox.Show($"Word '{selectedWord}' has been removed.", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            else
+            {
+                MessageBox.Show("Please select a word to remove.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void ConcatenateSelectedWords()
+        {
+            string selectedWord1 = comboBox1.SelectedItem as string;
+            string selectedWord2 = comboBox2.SelectedItem as string;
+            if (string.IsNullOrWhiteSpace(selectedWord1) || string.IsNullOrWhiteSpace(selectedWord2))
+            {
+                MessageBox.Show("Please select two different words.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            if (selectedWord1.Equals(selectedWord2))
+            {
+                MessageBox.Show("Please select different words for concatenation.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+            string concatenatedWord = selectedWord1 + selectedWord2;
+            concatenatedWords.Add(concatenatedWord);
+
+            MessageBox.Show($"Concatenated word '{concatenatedWord}' has been added to the collection.", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            lblConcatenate.Text = $"Concatenated Word: {concatenatedWord}";
+        }
+
+        private void radioButton2_CheckedChanged(object sender, EventArgs e)
+        {
+            btnRemoveItem.Text = "Concatenate Words";
+        }
+    }
+    
+}
